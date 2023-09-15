45f64943f87ae980f8acec953bcc6366340b9ce1
Craig Evans <craigevans83@gmail.com>
2023-09-15 12:03:11 +0100

    added component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadertronic zzz.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My React App</h1>
+      <MyComponent />
     </div>
   );
 }
---
+++ src/MyComponent.jsx
@@ -0,0 +1,13 @@
+// src/MyComponent.jsx
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello, I'm a React Component!</h1>
+      <p>This is my content.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
