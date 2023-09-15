7b1dd8c66202396781ee7716ff925b38354309cb
Craig Evans <craigevans83@gmail.com>
2023-09-15 10:58:23 +0100

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadertron.
+          Edit <code>src/App.js</code> and save to reloadertronic.
         </p>
         <a
           className="App-link"
