f8ed8d57cb541779a44f7911916f15c1892ebab8
Craig Evans <craigevans83@gmail.com>
2023-09-15 11:26:48 +0100

    added zzz

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadertronic.
+          Edit <code>src/App.js</code> and save to reloadertronic zzz.
         </p>
         <a
           className="App-link"
